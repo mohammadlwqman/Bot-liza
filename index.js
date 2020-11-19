288745e44dfc3960f8f70d53595e368e774c16d4
mohammadlwqman <74517472+mohammadlwqman@users.noreply.github.com>
2020-11-19 17:59:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -87,4 +87,4 @@ async function playSong(guild, song) {
     dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
 }
 
-client.login("Nzc4NjUzMjA2Mzk3NzE0NDUy.X7VHTw.D4F2vjRYdlKBoHPOnrfA5OxUO1U")
+client.login()
