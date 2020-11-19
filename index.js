7a37a85caa1d7d45f5d4c6764b5f283a97bb04f8
mohammadlwqman <74517472+mohammadlwqman@users.noreply.github.com>
2020-11-19 18:36:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,90 +1,81 @@
+//Packages
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const {prefix, token} = require("./config.json")
-
-var queue = new Map();
-
+const ms = require('ms');
+const fs = require('fs');
 const ytdl = require('ytdl-core');
+const cheerio = require('cheerio');
+const request = require('request');
+const Duration = require('humanize-duration');
+const config = require('./config.json');
 
-client.on('ready', () => console.log("ready"));
 
-client.on('message', async (message) => {
-    if(message.author.bot) return;
-    if(message.content.indexOf(prefix) !== 0) return;
+//clients
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+const Embed = new Discord.MessageEmbed();
+bot.commands = new Discord.Collection();
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
+const commandFiles = fs.readdirSync('./commands/').filter((file) => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
 
-    if(command == "play") {
-        if(!args[0]) return;
-        let url = args.join(" ");
-        if(!url.match(/(youtube.com|youtu.be)\/(watch)?(\?v=)?(\S+)?/)) return message.channel.send("Please provide a valid Youtube link!");
-
-        let serverQueue = queue.get(message.guild.id);
-        let vc = message.member.voice;
-
-        if(!vc) return message.channel.send("You are not in a voice channel!");
-
-        if(!vc.channel.permissionsFor(client.user).has('CONNECT') || !vc.channel.permissionsFor(client.user).has('SPEAK')) return message.channel.send("I do not have permission!");
-
-        let songinfo = await ytdl.getInfo(url);
-        let song = {
-            title: songinfo.title,
-            url: songinfo.video_url
-        }
-
-        if(!serverQueue) {
-            let queueConst = {
-                textChannel: message.channel,
-                voiceChannel: vc.channel,
-                connection: null,
-                songs: [],
-                volume: 5,
-                playing: true
-            };
-
-            queue.set(message.guild.id, queueConst);
-            queueConst.songs.push(song);
-
-            try {
-                let connection = await vc.channel.join();
-                queueConst.connection = connection
-                playSong(message.guild, queueConst.songs[0])
-            } catch (error) {
-                console.log(error);
-                queue.delete(message.guild.id);
-                return message.channel.send("There was an error playing the song! Error: " + error);
-            }
-        } else {
-            serverQueue.songs.push(song);
-            return message.channel.send(`${song.title} has been added to the queue!`)
-        }
-    }
-})
-
-/**
- * 
- * @param {Discord.Guild} guild 
- * @param {Object} song 
- */
-async function playSong(guild, song) {
-    let serverQueue = queue.get(guild.id);
-
-    if(!song){
-        serverQueue.voiceChannel.leave();
-        queue.delete(guild.id);
-        return;
-    }
-
-    const dispatcher = serverQueue.connection.play(ytdl(song.url)).on('end', () => {
-        serverQueue.songs.shift();
-        playSong(guild, serverQueue.songs[0]);
-    })
-    .on('error', () => {
-        console.log(error)
-    })
-
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    bot.commands.set(command.name, command);
 }
 
-bot.login(process.env.TOKEN)
+
+bot.on('ready', () => {
+    console.log('ACTIVE!');
+});
+
+bot.on('message', (message) => {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    let args = message.content.substring(config.prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ban') {
+        bot.commands.get('pp').execute(message, args, bot);
+    } else if (command === 'members') {
+        bot.commands.get('pp').execute(message, args, bot);
+    } else if (command === 'cls') {
+        bot.commands.get('cls').execute(message, args, bot);
+    } else if (command === 'ctc') {
+        bot.commands.get('ctc').execute(message, args, bot);
+    } else if (command === 'cvc') {
+        bot.commands.get('cvc').execute(message, args, bot);
+    } else if (command === 'echo') {
+        bot.commands.get('echo').execute(message, args, bot);
+    } else if (command === 'g') {
+        bot.commands.get('g').execute(message, args, bot);
+    } else if (command === 'gg') {
+        bot.commands.get('gg').execute(message, args, bot, Discord, Duration);
+    } else if (command === 'help') {
+        bot.commands.get('help').execute(message, args, Discord, Duration);
+    } else if (command === 'i') {
+        bot.commands.get('i').execute(message, args, cheerio);
+    } else if (command === 'iq') {
+        bot.commands.get('iq').execute(message, args, bot);
+    } else if (command === 'kick') {
+        bot.commands.get('kick').execute(message, args, bot);
+    } else if (command === 'members') {
+        bot.commands.get('members').execute(message, args, bot);
+    } else if (command === 'pick') {
+        bot.commands.get('pick').execute(message, args, bot, Discord);
+    } else if (command === 'ping') {
+        bot.commands.get('ping').execute(message, args, bot);
+    } else if (command === 'pl') {
+        bot.commands.get('pl').execute(message, args, bot);
+    } else if (command === 'pm') {
+        bot.commands.get('pm').execute(message, args, bot);
+    } else if (command === 'poll') {
+        bot.commands.get('poll').execute(message, args, Embed);
+    } else if (command === 'pp') {
+        bot.commands.get('pp').execute(message, args, bot);
+    } else if (command === 'spam') {
+        bot.commands.get('spam').execute(message, args, bot);
+    } else if (command === 'status') {
+        bot.commands.get('status').execute(message, args, bot);
+    } 
+});
+
+bot.login(config.token);
